ef05036f27f3ef9fdb796e8b7d99d42168b741fc
Jitendra soni <84441277+JitendraSoni1234@users.noreply.github.com>
2023-04-21 11:16:24 +0530

    code fix

--- src/pages/DetailSection.jsx
+++ src/pages/DetailSection.jsx
@@ -13,9 +13,11 @@ function DetailSection() {
   };
 
   const handleIpSubmit = () => {
-    getIpData(inputData.trim());
-    setInputData('');
-  }
+    if (inputData.length) {
+      getIpData(inputData.trim());
+    }
+    setInputData("");
+  };
 
   return (
     <section className="xl:bg-desktopImage bg-mobileImage bg-cover flex flex-col gap-6 justify-center place-items-center h-[250px]">
@@ -27,7 +29,10 @@ function DetailSection() {
           className="focus:outline-none rounded-xl p-3 h-[55px] w-[340px] xl:w-[550px] placeholder:text-dark-gray placeholder:font-semiBold"
           placeholder="Search for any IP Address or domain"
         />
-        <button disabled={isLoading} onClick={handleIpSubmit} className="bg-black p-3 h-[55px] xl:w-[60px] flex justify-center place-items-center rounded-tr-xl rounded-br-xl absolute right-0 top-0">
+        <button
+          disabled={isLoading}
+          onClick={handleIpSubmit}
+          className="bg-black p-3 h-[55px] xl:w-[60px] flex justify-center place-items-center rounded-tr-xl rounded-br-xl absolute right-0 top-0">
           <img src={arrow} alt="search" />
         </button>
       </div>
