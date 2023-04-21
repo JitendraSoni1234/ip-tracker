c0a9adb408ed1164b07c60da60daa48286388a5e
Jitendra soni <84441277+JitendraSoni1234@users.noreply.github.com>
2023-04-21 11:12:10 +0530

    completed ip tracker

---
+++ src/app/api/ipSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const ipSlice = createApi({
+  reducerPath: "ipSlice",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://geo.ipify.org/api/v2/" }),
+  endpoints: builder => ({
+    getIpData: builder.query({
+      query: data => {
+        return {
+          url: "country,city",
+          params: {
+            apiKey: "at_ABlZ5bcjMpJ3xBZHPSJ5tVjOurkvo",
+            ipAddress: data,
+          },
+        };
+      },
+    }),
+  }),
+});
+
+const ipReducer = createSlice({
+  name: "ipReducer",
+  initialState: { loading: false, result: {}, error: {}, isError: false },
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addMatcher(ipSlice.endpoints.getIpData.matchPending, state => {
+        state.loading = true;
+      })
+      .addMatcher(ipSlice.endpoints.getIpData.matchFulfilled, (state, action) => {
+        state.loading = false;
+        state.result = action.payload;
+      })
+      .addMatcher(ipSlice.endpoints.getIpData.matchRejected, (state, action) => {
+        state.loading = false;
+        state.isError = true;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default ipReducer.reducer;
+export const { useLazyGetIpDataQuery } = ipSlice;
---
+++ src/pages/DetailSection.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import arrow from "/icon-arrow.svg";
+import DetailCard from "../components/DetailCard";
+import { useLazyGetIpDataQuery } from "../app/api/ipSlice";
+
+function DetailSection() {
+  const [inputData, setInputData] = useState("");
+  const [getIpData, { isLoading }] = useLazyGetIpDataQuery();
+
+  const handleChange = e => {
+    e.preventDefault();
+    setInputData(e.target.value);
+  };
+
+  const handleIpSubmit = () => {
+    getIpData(inputData.trim());
+    setInputData('');
+  }
+
+  return (
+    <section className="xl:bg-desktopImage bg-mobileImage bg-cover flex flex-col gap-6 justify-center place-items-center h-[250px]">
+      <div className="text-white text-3xl font-semiBold">IP Address Tracker</div>
+      <div className="relative">
+        <input
+          value={inputData}
+          onChange={handleChange}
+          className="focus:outline-none rounded-xl p-3 h-[55px] w-[340px] xl:w-[550px] placeholder:text-dark-gray placeholder:font-semiBold"
+          placeholder="Search for any IP Address or domain"
+        />
+        <button disabled={isLoading} onClick={handleIpSubmit} className="bg-black p-3 h-[55px] xl:w-[60px] flex justify-center place-items-center rounded-tr-xl rounded-br-xl absolute right-0 top-0">
+          <img src={arrow} alt="search" />
+        </button>
+      </div>
+      <DetailCard />
+    </section>
+  );
+}
+
+export default DetailSection;
